8815b9e1433f156dcbf6721dfb3858939a78b162
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:42 -0800

    feat: Remove orphaned HTML files
    Automatically delete any HTML files in the output directory that don't
    have a Markdown source file (happens when renaming/moving Markdown files
    around)

--- markdown_to_html.sh
+++ markdown_to_html.sh
@@ -4,12 +4,23 @@ PANDOC_ARGS="-f markdown+lists_without_preceding_blankline --katex --highlight-s
 N=24 # Number of parallel Pandoc processes
 function convert_directory {
 	INPUT_DIR="${1}"
+	# Convert Markdown to HTML
 	for INPUT_FILE in "${INPUT_DIR}/markdown/"*
 	do
 		((i=i%N)); ((i++==0)) && wait
 		OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
 		OUTPUT_FILE="${OUTPUT_FILE%.*}.html"
 		eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS} -o \"${INPUT_DIR}/${OUTPUT_FILE}\""&
+	done
+	# Delete orphaned HTML files
+	for HTML_FILE in "${INPUT_DIR}/"*.html
+	do
+		MARKDOWN_FILE=$(basename -- "${HTML_FILE}")
+		MARKDOWN_FILE="${INPUT_DIR}/markdown/${MARKDOWN_FILE%.*}.md"
+		if [ ! -f "${MARKDOWN_FILE}" ];
+		then
+			rm "${HTML_FILE}"
+		fi
 	done
 }
 convert_directory "cs1300"&
