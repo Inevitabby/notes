cb2116436952eacfbf9c06e3b111d89d13994ea0
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:43 -0800

    fix: Only run *.md files through converter

--- daemon.sh
+++ daemon.sh
@@ -35,7 +35,7 @@ trap "cleanup" SIGINT
 function convert {
 	# Convert notes
 	for INPUT_DIR in "${DIRECTORIES[@]}"; do
-		for INPUT_FILE in "${INPUT_DIR}/markdown/"*
+		for INPUT_FILE in "${INPUT_DIR}/markdown/"*.md
 		do
 			((i=i%N)); ((i++==0)) && wait # Limit number of parallel processes
 			convert_markdown "${INPUT_FILE}" "${INPUT_DIR}"
