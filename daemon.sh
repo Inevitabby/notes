03866f020b73ad1787489dc6b110b92987eefd33
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:45 -0800

    tweak: Change parallel processes from 24 -> 32
    After doing a couple benchmarks, 32 appears to be a sweet spot.

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
 DIRECTORIES=("cs1300" "cs1400" "cs2400" "cs2600" "cs2640" "cs50" "phl2020" "mat1150")
-N=24 # Number of parallel Pandoc processes
+N=32 # Number of parallel Pandoc processes
 BASE_PANDOC_ARGS="-f markdown+lists_without_preceding_blankline --katex --highlight-style=pygments --wrap=preserve --standalone --quiet --template template.html --lua-filter=.util/plantuml.lua"
 STYLE_FILE="style.min.css"
 RUNNING=true
