9b55b9997d661c1db1efdf2a2ac0d5e36638faaa
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:42 -0800

    delete: Leftover files from move

--- markdown_to_html.sh
+++ markdown_to_html.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
-INPUT_DIR="markdown"
+INPUT_DIR="cs1300"
 PANDOC_ARGS="-f markdown+lists_without_preceding_blankline --katex --highlight-style=pygments --wrap=preserve --standalone"
-for INPUT_FILE in "$INPUT_DIR"/*
+for INPUT_FILE in "${INPUT_DIR}/markdown/"*
 do
 	OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
 	OUTPUT_FILE="${OUTPUT_FILE%.*}.html"
-	eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS} -o \"${OUTPUT_FILE}\""
+	eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS} -o \"${INPUT_DIR}/${OUTPUT_FILE}\""
 done
