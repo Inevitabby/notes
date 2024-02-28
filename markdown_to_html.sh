f37fdd91ddb077b9470021608c8e46fd4fb8fb0a
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:42 -0800

    feat: Convert notes in multiple directories
    Makes it easier to add more notes in the future.

--- markdown_to_html.sh
+++ markdown_to_html.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
-INPUT_DIR="cs1300"
 PANDOC_ARGS="-f markdown+lists_without_preceding_blankline --katex --highlight-style=pygments --wrap=preserve --standalone"
-for INPUT_FILE in "${INPUT_DIR}/markdown/"*
-do
-	OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
-	OUTPUT_FILE="${OUTPUT_FILE%.*}.html"
-	eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS} -o \"${INPUT_DIR}/${OUTPUT_FILE}\""
-done
+function convert_directory {
+	INPUT_DIR="${1}"
+	for INPUT_FILE in "${INPUT_DIR}/markdown/"*
+	do
+		OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
+		OUTPUT_FILE="${OUTPUT_FILE%.*}.html"
+		eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS} -o \"${INPUT_DIR}/${OUTPUT_FILE}\""
+	done
+}
+convert_directory "cs1300"
+convert_directory "cs1400"
