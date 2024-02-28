0c12e421f3d3fb5a2ae49ef309fd98cbcffe0c07
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:45 -0800

    feat: Dynamic PlantUML SVG colors
    Hacked-in some dynamic CSS variables into the SVGs generated by PlantUML
    to make PlantUML diagrams look seamless in dark and light theme.

--- .util/plantuml.lua
+++ .util/plantuml.lua
@@ -3,9 +3,25 @@ local function convert(uml)
   local final = pandoc.pipe("plantuml", {"-tsvg", "-pipe", "-charset", "UTF8"}, uml)
   return final
 end
+-- Hack-in CSS variables to create dynamic SVG colors
+local function colorHack(svg)
+  local cssVariables = {
+    ["var(--background-color)"] = "#FFFFFF",
+    ["var(--TOC-background-color)"] = "#F1F1F1",
+    ["var(--blockquote-text-color)"] = "#181818",
+    ["var(--text-color)"] = "#000000",
+    ["var(--svg-green)"] = "#ADD1B2",
+    ["var(--svg-purple)"] = "#B4A7E5",
+  }
+  for var, hex in pairs(cssVariables) do
+    svg = svg:gsub(hex, var)
+  end
+  return svg
+end
 function CodeBlock(block)
   if block.classes[1] == "plantuml" then
     local svg = convert(block.text)
+    svg = colorHack(svg)
     return pandoc.RawInline("html", "<center>" .. svg .. "</center>")
   end
 end
