050b5e3dbc08690eb64ad0b67c2c997d22b90a8c
Inevitabby <inevitabby@cock.li>
2024-02-28 12:18:43 -0800

    feat: Only convert changed Markdown files
    Doing `watch bash markdown_to_html.sh` will no longer max out CPU usage.

---
+++ check_hash.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+function check_hash {
+	INPUT_FILE="${1}"
+	OUTPUT_DIR="${2}"
+	HASH_DIR=".md5sums/${OUTPUT_DIR}" && mkdir -p "${HASH_DIR}"
+	HASH_FILE="${HASH_DIR}/$(basename -- "${INPUT_FILE}").hash"
+	CURRENT_HASH=$(md5sum "$INPUT_FILE" | cut -d' ' -f1)
+	PREVIOUS_HASH=""
+	[ -f "$HASH_FILE" ] && PREVIOUS_HASH=$(cat "$HASH_FILE")
+	if [ "$CURRENT_HASH" == "$PREVIOUS_HASH" ]; then
+		return 0  # Hash unchanged
+	else
+		echo "$CURRENT_HASH" >"$HASH_FILE"
+		return 1  # Hash changed
+	fi
+}
--- markdown_to_html.sh
+++ markdown_to_html.sh
@@ -2,33 +2,37 @@
 cd "$(dirname "$0")" || exit
 BASE_PANDOC_ARGS="-f markdown+lists_without_preceding_blankline --katex --highlight-style=pygments --wrap=preserve --standalone --quiet --template template.html"
 N=24 # Number of parallel Pandoc processes
+source check_hash.sh
 function convert_directory {
 	INPUT_DIR="${1}"
 	# Convert Markdown to minified HTML
 	for INPUT_FILE in "${INPUT_DIR}/markdown/"*
 	do
 		((i=i%N)); ((i++==0)) && wait
+		# Skip conversion if Markdown file hasn't changed
+		if check_hash "${INPUT_FILE}" "${INPUT_DIR}"; then
+			continue
+		fi
+		# Determine name of output file
 		OUTPUT_FILE=$(basename -- "${INPUT_FILE}")
 		OUTPUT_FILE="${OUTPUT_FILE%.*}.html"
+		# Add a Table of Contents to all non-index files
 		PANDOC_ARGS="${BASE_PANDOC_ARGS}"
 		if [ "$OUTPUT_FILE" != "index.html" ]; then
 			PANDOC_ARGS+=" --toc"
 		fi
-		(
-		# Convert Markdown to HTML
-		eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS}" |
-		awk -f "minify.awk"
-		) > "${INPUT_DIR}/${OUTPUT_FILE}" &
-
-# The above command is run in the background using "&"
+		# Convert Markdown to minified HTML
+		(eval pandoc "-i \"${INPUT_FILE}\" ${PANDOC_ARGS}" | awk -f "minify.awk") > "${INPUT_DIR}/${OUTPUT_FILE}" &
 	done
-	# Delete orphaned HTML files
+	# Delete orphaned HTML files and hashes
 	for HTML_FILE in "${INPUT_DIR}/"*.html
 	do
 		MARKDOWN_FILE=$(basename -- "${HTML_FILE}")
 		MARKDOWN_FILE="${INPUT_DIR}/markdown/${MARKDOWN_FILE%.*}.md"
 		if [ ! -f "${MARKDOWN_FILE}" ]; then
 			rm "${HTML_FILE}"
+			HASH_FILE=".md5sums/${INPUT_DIR}/$(basename -- "${MARKDOWN_FILE}").hash"
+			[ -f "$HASH_FILE" ] && rm "${HASH_FILE}"
 		fi
 	done
 }
