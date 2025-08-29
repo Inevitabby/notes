463f8ddde1b36aa0aca7c00cd661f610299b960d
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2025-08-28 21:51:17 -0700

    feat: Give first-top level header the "title" class if needed
    If meta.title isn't set, we promote the first <h1> in the file to
    a <h1 class="title"> so that all the CSS can apply to it.

--- .util/filters/title.lua
+++ .util/filters/title.lua
@@ -14,9 +14,27 @@ function Meta(meta)
     meta.title = pandoc.MetaString(first_header)
     meta.noheader = pandoc.MetaString("true")
   end
-  -- Special Case: index.md
-  -- if current_file_name == "index.md" then
-  --   meta.noheader = pandoc.MetaString("true")
-  -- end
   return meta
 end
+-- Promote the first top-level Header to h1.title if it's the meta.title
+function Pandoc(doc)
+  -- Exit if no meta.title
+  if not doc.meta.title then
+    return nil
+  end
+  -- Get meta.title
+  local meta_title_text = pandoc.utils.stringify(doc.meta.title)
+  -- Find the first top-level header
+  for _, el in ipairs(doc.blocks) do
+    if el.t == 'Header' and el.level == 1 then
+      local header_text = pandoc.utils.stringify(el)
+      -- Promote if == meta.title
+      if header_text == meta_title_text then
+        el.classes:insert('title')
+      end
+      break
+    end
+  end
+  return doc
+end
+
