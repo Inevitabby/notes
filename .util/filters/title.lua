2b9931705f02dd4e3917871205844aefabbca5f6
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2025-08-31 19:23:02 -0700

    feat: Append " ($course$)" to child notes

--- .util/filters/title.lua
+++ .util/filters/title.lua
@@ -6,12 +6,20 @@ function Header(el)
     first_header = pandoc.utils.stringify(el.content)
   end
 end
--- Set meta.title if missing
+-- Set title
 function Meta(meta)
+  -- Set meta.title if missing
   if not meta.title and first_header then
     meta.title = pandoc.MetaString(first_header)
     meta.noheader = pandoc.MetaString("true")
     did_promotion = true
+  end
+  -- Set meta.course (used in template.html)
+  local current_file_path = PANDOC_STATE.input_files[1]
+  local current_file_name = current_file_path:match("([^/]+)$")
+  local course = pandoc.path.directory(current_file_path)
+  if current_file_name ~= "index.md" and course ~= "." then
+    meta.course = pandoc.MetaString(string.upper(course))
   end
   return meta
 end
